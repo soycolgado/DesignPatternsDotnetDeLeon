67fd896a98024825c57ea6b874aa37b446c3d87f
Nestor Bardel <nestorbardel@gmail.com>
2025-08-31 12:34:27 -0300

    Agregar migración inicial para crear tablas `Brand` y `Beer` en la base de datos. Actualizar configuración de conexión en `DesignPatternsContext`.

---
+++ DesignPatterns.Models/Migrations/20250831151255_InitialCreate.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DesignPatterns.Models.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Brand",
+                columns: table => new
+                {
+                    BrandId = table.Column<Guid>(type: "uniqueidentifier", nullable: false, defaultValueSql: "(newid())"),
+                    Name = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("Brand_pk", x => x.BrandId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Beer",
+                columns: table => new
+                {
+                    BeerId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false),
+                    Style = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false),
+                    BrandId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("Beer_pk", x => x.BeerId);
+                    table.ForeignKey(
+                        name: "Beer_Brand__fk",
+                        column: x => x.BrandId,
+                        principalTable: "Brand",
+                        principalColumn: "BrandId");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Beer_BrandId",
+                table: "Beer",
+                column: "BrandId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Beer");
+
+            migrationBuilder.DropTable(
+                name: "Brand");
+        }
+    }
+}
